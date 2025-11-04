e5916c1995738dc42b0d63021db5953f450f7247
Jonah Lee <jonahjacqueslee@gmail.com>
2025-11-03 21:36:17 -0800

    Switch to typescript and add interactive leaflet map

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import type { LatLngTuple } from 'leaflet'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
+const position: LatLngTuple = [51.505, -0.09,]
+
+export default function App(){
+  return (
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{height: "100vh", width: "100vw"}}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br />
+        </Popup>
+      </Marker>
+    </MapContainer>
+  )
+}
